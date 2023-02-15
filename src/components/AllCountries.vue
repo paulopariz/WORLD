1352239c79034b93c0ae4b6e64480e787daf8826
paulopariz <paulopariz01@gmail.com>
2023-02-15 01:16:17 -0300

    nav/theme

--- src/components/AllCountries.vue
+++ src/components/AllCountries.vue
@@ -1,28 +1,38 @@
 <template>
-  <div class="container mt-36">
-    <div class="grid grid-cols-4 gap-16">
-      <div v-for="pais in paises" :key="pais" class="w-72 rounded-lg">
-        <div class="">
-          <img :src="pais.flags.png" class="rounded-lg rounded-b-none h-44 w-72" />
-        </div>
-        <div class="border-2 border-t-0 border-slate-200 p-6">
-          <h1 class="text-lg font-semibold">{{ pais.name.common }}</h1>
-
-          <div class="mt-4 flex flex-col gap-1">
-            <p class="text-sm text-gray-900">
-              <span class="font-semibold text-black">Região:</span>
-              {{ pais.region }}
-            </p>
-
-            <p class="text-sm text-gray-900">
-              <span class="font-semibold text-black">Capital:</span>
-              {{ pais.capital + "" }}
-            </p>
-
-            <p class="text-sm text-gray-900">
-              <span class="font-semibold text-black">População:</span>
-              {{ pais.population.toLocaleString("US") }}
-            </p>
+  <div>
+    <NavBar />
+    <div class="container mt-16">
+      <div class="grid grid-cols-4 gap-16">
+        <div
+          v-for="pais in paises"
+          :key="pais"
+          class="w-72 rounded-lg border-2 border-t-0 border-base-300 bg-base-200 shadow-2xl"
+        >
+          <div class="">
+            <img :src="pais.flags.png" class="rounded-lg rounded-b-none h-44 w-72" />
+          </div>
+          <div class="p-6">
+            <h1 class="text-lg font-semibold text-accent-content">
+              {{ pais.name.common }}
+            </h1>
+            <div class="mt-4 flex flex-col gap-1">
+              <p class="text-sm text-base-content">
+                <span class="font-semibold text-accent-content">Região:</span>
+                {{ pais.region }}
+              </p>
+              <p class="text-sm text-base-content">
+                <span class="font-semibold text-accent-content">Capital:</span>
+                {{ pais.capital + "" }}
+              </p>
+              <p class="text-sm text-base-content">
+                <span class="font-semibold text-accent-content">População:</span>
+                {{ pais.population.toLocaleString("US") }}
+              </p>
+              <p class="text-sm text-base-content">
+                <span class="font-semibold text-accent-content">Área territorial:</span>
+                {{ pais.area.toLocaleString("US") }} km²
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -31,19 +41,33 @@
 </template>
 
 <script>
+import NavBar from "./NavBar.vue";
+
 export default {
   name: "AllCountries",
+  components: { NavBar },
 
   data() {
     return {
       paises: [],
+      link: "https://restcountries.com/v3.1/all",
     };
   },
 
   mounted() {
-    fetch("https://restcountries.com/v3.1/all")
+    fetch(this.link)
       .then((response) => response.json())
       .then((data) => (this.paises = data));
+  },
+
+  methods: {
+    xxx() {
+      this.link = "https://restcountries.com/v3.1/region/europe";
+
+      fetch(this.link)
+        .then((response) => response.json())
+        .then((data) => (this.paises = data));
+    },
   },
 };
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-20 shadow-md">
+    <div class="py-6 container">
+      <div>
+        <img src="../assets/img/world.svg" class="w-8 invert" />
+      </div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style></style>
