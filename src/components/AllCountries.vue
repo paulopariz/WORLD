8262c81d6f2e258f7dc107b5f027631ddcdbac14
paulopariz <paulopariz01@gmail.com>
2023-02-15 02:22:49 -0300

    get europe

--- src/components/AllCountries.vue
+++ src/components/AllCountries.vue
@@ -2,7 +2,38 @@
   <div>
     <NavBar />
     <div class="container mt-16">
-      <div class="grid grid-cols-4 gap-16">
+      <div class="flex justify-between items-center">
+        <input
+          type="search"
+          placeholder="Pesquise por algum país"
+          class="input w-full h-16 input-bordered border-2 border-base-300 max-w-md shadow-xl rounded-lg text-base tracking-wider font-normal placeholder:text-accent-content bg-base-200"
+        />
+
+        <div class="dropdown dropdown-end">
+          <label
+            tabindex="0"
+            class="btn h-16 bg-base-200 hover:bg-base-300 hover:border-base-200 border-2 border-base-300 text-base rounded-lg shadow-xl normal-case tracking-wider font-normal text-accent-content w-72 flex items-center gap-9"
+            >Filtrar por região
+            <img src="../assets/img/arrow.svg" class="w-3 mt-1.5 invert"
+          /></label>
+          <ul
+            tabindex="0"
+            class="dropdown-content mt-2 menu p-2 bg-base-200 border-2 border-base-300 rounded-lg w-72 shadow-2xl"
+          >
+            <li><a class="hover:bg-base-300 active:bg-base-100">Africa</a></li>
+            <li><a class="hover:bg-base-300 active:bg-base-100">America</a></li>
+            <li><a class="hover:bg-base-300 active:bg-base-100">Asia</a></li>
+            <li>
+              <a @click="clickEurope" class="hover:bg-base-300 active:bg-base-100"
+                >Europa</a
+              >
+            </li>
+            <li><a class="hover:bg-base-300 active:bg-base-100">Oceania</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-4 gap-16 mt-16">
         <div
           v-for="pais in paises"
           :key="pais"
@@ -61,12 +92,14 @@ export default {
   },
 
   methods: {
-    xxx() {
+    clickEurope() {
       this.link = "https://restcountries.com/v3.1/region/europe";
 
-      fetch(this.link)
-        .then((response) => response.json())
-        .then((data) => (this.paises = data));
+      setTimeout(() => {
+        fetch(this.link)
+          .then((response) => response.json())
+          .then((data) => (this.paises = data));
+      }, 2000);
     },
   },
 };
